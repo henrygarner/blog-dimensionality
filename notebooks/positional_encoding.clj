12471100ca00e9c87d9301cbcf054ed8271e8aa0
Henry Garner <hga@airhawk.local>
2024-10-11 14:51:07 +0100

    Visualise positional encoding

--- notebooks/positional_encoding.clj
+++ notebooks/positional_encoding.clj
@@ -1,31 +1,51 @@
 (ns positional-encoding
   (:require [nextjournal.clerk :as clerk]
-            [clojure.core.matrix :as m]
-            [clojure.core.matrix.operators :as op :refer [*=]]
-            [clojure.core.matrix.linear :as l]))
+            [kixi.stats.core :as k]
+            [kixi.stats.math :as km]))
 
-(defn synthetic-data
-  [nx ny]
-  (for [x (range nx) y (range ny)]
-    {:x x :y y :temp (rand)}))
+(defn make-vectoriser
+  [k]
+  (let [d (* k 2)]
+    (fn [t]
+      (->> (for [i (range k)
+                 :let [wk (/ 1.0 (km/pow 10000 (/ i k)))
+                       wkt (* wk t)]]
+             (vector (km/sin wkt)
+                     (km/cos wkt)))
+           (apply concat)
+           (vec)))))
+
+(def vectoriser
+  (make-vectoriser 100))
+
+(vectoriser 1)
+
+(vectoriser 100)
 
 (def data
-  (synthetic-data 250 250))
+  (for [t (range 1000)
+        :let [v (vectoriser t)]
+        [i x] (map-indexed vector v)]
+    {:x i :y t :val x}))
 
 (clerk/vl
  {:data {:values data},
-  :title "Example heatmap",
-  :config {:view {:strokeWidth 0, :step 2},
+  :title "Positional encoding for token offsets",
+  :config {:view {:strokeWidth 0, :step 3},
            :axis {:domain false}},
   :mark "rect",
   :encoding
   {:x
    {:field "x",
-    :type "ordinal"},
+    :type "ordinal"
+    :axis {:values (range 0 1000 10)
+           :title "Index into positional vector"}},
    :y
    {:field "y",
-    :type "ordinal"},
+    :type "ordinal"
+    :axis {:values (range 0 1000 10)
+           :title "Token offset"}},
    :color
-   {:field "temp",
+   {:field "val",
     :type "quantitative", 
     :legend {:title "Example legend"}}}})
